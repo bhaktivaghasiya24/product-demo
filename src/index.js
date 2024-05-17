8fce62cdbd452d0f74406d3c8cc018b557d77a14
bhaktivaghasiya24 <bhaktivaghasiya24@gmail.com>
2024-05-17 17:50:27 +0530

    product demo

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './features/counter/Home/home';
+
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+        <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+              <Route path= "/" element={<Home />} />
+              <Route path="/product/:productId" element={<h1>product Details</h1>} />
+              {/* <Route path="/login" element={<Login />} /> */}
+              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+              <Route path="*" element={<h1>Page not found</h1>} />  
+          </Routes>
+        </BrowserRouter>
+        </Provider>
+    );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+  
